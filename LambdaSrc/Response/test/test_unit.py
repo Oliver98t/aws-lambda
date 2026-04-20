ed5e0f51b58c3819ff53d5e9b935803fdcbc853b
Oliver98t <oli1998t@gmail.com>
2026-04-20 15:02:29 +0100

    Updated unit tests

--- LambdaSrc/Response/test/test_unit.py
+++ LambdaSrc/Response/test/test_unit.py
@@ -8,20 +8,20 @@ def test_generate_response():
 event_test_data = {
     "Records": [
         {
-            "messageId": "033275d9-4fc5-493e-94ec-a9544127e6de",
-            "receiptHandle": "AQEBF2SQZQETISakK96XdRNxC8MNhjLSUZwW0aqzWLJmttP40XDaMCqPWJ2v4BSWZc1dY7dtd5mb2w/rCU3zSsJp5SV4LRwF4SoeXiXPu0exxWZp8StNphJPEIH3DclfOINXk31chS3N0MOqCOcaqwUKXnK8oqi3LJSLj1a+Jzy63AVfx42l338VgrGtBruPzNaAniZfjz9BsI8dyTIF4NahcKRAksNxTL9qSY645Bp1q/HbLKmpZyHb3Bj4FTrmeQOlo9dwyUNwhXib74O2agfRkJZR8YSbAa7KqTe0nkDXvfeIXKr6yQAi9YCMuzY+odmLSzKJat1QTY3DWcDxQDGsySgC/53QnavtvpzcABEnS6W9f6wbPcEnQE2qJwunfHbDv87dD6iAdSjuJRMFIUwp6w==",
+            "messageId": "12345678-1234-1234-1234-123456789012",
+            "receiptHandle": "test-receipt-handle-12345",
             "body": '{"user": "test", "transcription": "Give me a series of Python interview questions."}',
             "attributes": {
                 "ApproximateReceiveCount": "1",
-                "AWSTraceHeader": "Root=1-69e61a45-41a85f322124961a0ed0b178;Parent=0aa04879a227ce3f;Sampled=0;Lineage=1:5d53ea41:0",
-                "SentTimestamp": "1776687697356",
-                "SenderId": "AROAVCGYG6IT52M4VXCMJ:SpeechToText_dev",
-                "ApproximateFirstReceiveTimestamp": "1776687697361",
+                "AWSTraceHeader": "Root=1-00000000-0000000000000000;Parent=0000000000000000;Sampled=0;Lineage=1:0000000000:0",
+                "SentTimestamp": "1000000000000",
+                "SenderId": "AIDACKCEVSQ6C2EXAMPLE:test-role",
+                "ApproximateFirstReceiveTimestamp": "1000000000000",
             },
             "messageAttributes": {},
-            "md5OfBody": "b6469447f4f97c0b869a1b7cb921f1c6",
+            "md5OfBody": "0000000000000000000000000000000000000000",
             "eventSource": "aws:sqs",
-            "eventSourceARN": "arn:aws:sqs:eu-west-2:348345791015:AInterview_dev_queue",
+            "eventSourceARN": "arn:aws:sqs:eu-west-2:000000000000:test-interview-queue",
             "awsRegion": "eu-west-2",
         }
     ]
--- LambdaSrc/SpeechToText/test/test_unit.py
+++ LambdaSrc/SpeechToText/test/test_unit.py
@@ -9,57 +9,57 @@ def test_transcribe():
     assert transcription == test_trasncription
 
 event_test_data = {
-        "version": "2.0",
+    "version": "2.0",
+    "routeKey": "$default",
+    "rawPath": "/",
+    "rawQueryString": "user=test",
+    "headers": {
+        "x-amzn-tls-version": "TLSv1.3",
+        "x-amz-date": "20260420T122125Z",
+        "x-forwarded-proto": "https",
+        "postman-token": "test-token-12345",
+        "x-forwarded-port": "443",
+        "x-forwarded-for": "127.0.0.1",
+        "accept": "*/*",
+        "x-amzn-tls-cipher-suite": "TLS_AES_128_GCM_SHA256",
+        "x-amzn-trace-id": "Root=1-00000000-0000000000000000",
+        "host": "test-lambda-id.lambda-url.eu-west-2.on.aws",
+        "cache-control": "no-cache",
+        "accept-encoding": "gzip, deflate, br",
+        "user-agent": "test-client/1.0",
+    },
+    "queryStringParameters": {"user": "test"},
+    "requestContext": {
+        "accountId": "000000000000",
+        "apiId": "testlambdaid",
+        "authorizer": {
+            "iam": {
+                "accessKey": "AKIAIOSFODNN7EXAMPLE",
+                "accountId": "000000000000",
+                "callerId": "AIDAEXAMPLE",
+                "cognitoIdentity": None,
+                "principalOrgId": None,
+                "userArn": "arn:aws:iam::000000000000:user/test-user",
+                "userId": "AIDAEXAMPLE",
+            }
+        },
+        "domainName": "test-lambda-id.lambda-url.eu-west-2.on.aws",
+        "domainPrefix": "testlambdaid",
+        "http": {
+            "method": "GET",
+            "path": "/",
+            "protocol": "HTTP/1.1",
+            "sourceIp": "127.0.0.1",
+            "userAgent": "test-client/1.0",
+        },
+        "requestId": "00000000-0000-0000-0000-000000000000",
         "routeKey": "$default",
-        "rawPath": "/",
-        "rawQueryString": "user=test",
-        "headers": {
-            "x-amzn-tls-version": "TLSv1.3",
-            "x-amz-date": "20260420T122125Z",
-            "x-forwarded-proto": "https",
-            "postman-token": "ab03b80a-a27b-486d-8ebf-1e0f42351d78",
-            "x-forwarded-port": "443",
-            "x-forwarded-for": "213.120.89.156",
-            "accept": "*/*",
-            "x-amzn-tls-cipher-suite": "TLS_AES_128_GCM_SHA256",
-            "x-amzn-trace-id": "Root=1-69e61a45-41a85f322124961a0ed0b178",
-            "host": "crje4j5cnd2yyhgbgpautdxrxm0qofor.lambda-url.eu-west-2.on.aws",
-            "cache-control": "no-cache",
-            "accept-encoding": "gzip, deflate, br",
-            "user-agent": "PostmanRuntime/7.53.0",
-        },
-        "queryStringParameters": {"user": "test"},
-        "requestContext": {
-            "accountId": "348345791015",
-            "apiId": "crje4j5cnd2yyhgbgpautdxrxm0qofor",
-            "authorizer": {
-                "iam": {
-                    "accessKey": "AKIAVCGYG6ITSJW4JKVE",
-                    "accountId": "348345791015",
-                    "callerId": "AIDAVCGYG6IT6WHIS63FU",
-                    "cognitoIdentity": None,
-                    "principalOrgId": None,
-                    "userArn": "arn:aws:iam::348345791015:user/oli-work",
-                    "userId": "AIDAVCGYG6IT6WHIS63FU",
-                }
-            },
-            "domainName": "crje4j5cnd2yyhgbgpautdxrxm0qofor.lambda-url.eu-west-2.on.aws",
-            "domainPrefix": "crje4j5cnd2yyhgbgpautdxrxm0qofor",
-            "http": {
-                "method": "GET",
-                "path": "/",
-                "protocol": "HTTP/1.1",
-                "sourceIp": "136.226.167.104",
-                "userAgent": "PostmanRuntime/7.53.0",
-            },
-            "requestId": "a441f224-f797-4ec5-a0c8-f673a3cd49d7",
-            "routeKey": "$default",
-            "stage": "$default",
-            "time": "20/Apr/2026:12:21:25 +0000",
-            "timeEpoch": 1776687685938,
-        },
-        "isBase64Encoded": False,
-    }
+        "stage": "$default",
+        "time": "20/Apr/2026:12:21:25 +0000",
+        "timeEpoch": 1000000000000,
+    },
+    "isBase64Encoded": False,
+}
 
 def test_handler(monkeypatch):
     monkeypatch.setattr(stt, "S3_BUCKET", "ainterviewupload")
